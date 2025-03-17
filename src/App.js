845fe126d8bf5aa543829531630c4652b695cf2d
viXa221 <vixa2122@gmail.om>
2025-03-17 12:38:32 +0200

    adding Username and Password

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>rwar</p>
+        <p placeholder="">Моля, въведете Username и Password долу:</p>
+        <p>Име:</p>
+        <button onclick="clickMe()" type="button" placeholder="">въведи!</button>
+        <p>Парола:</p>
+        <button onclick="clickMe()" type="text" placeholder="">въведи!</button>
+        <br></br>
+      
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://support.google.com/accounts/troubleshooter/2402620?hl=en"
+          href="mailto:someone@example.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <p>При проблем, натисни тук</p>
+                    <p>Мейл за въпроси!</p>
         </a>
       </header>
     </div>
